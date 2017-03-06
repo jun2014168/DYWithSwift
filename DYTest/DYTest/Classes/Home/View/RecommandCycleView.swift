2dd6ce041e549c554b51cfe8e79a5650d797c735
jun2014168 <niqingjun@niqingjundeMac-mini.local>
2017-03-06 21:38:40 +0800

    无线滚动

--- DYTest/DYTest/Classes/Home/View/RecommandCycleView.swift
+++ DYTest/DYTest/Classes/Home/View/RecommandCycleView.swift
@@ -11,6 +11,9 @@ import UIKit
 private let kCycleViewCellID = "kCycleViewCellID"
 
 class RecommandCycleView: UIView {
+    
+    // 定时器
+    var timer : Timer?
 
     var cycleModels : [CycleModel]?{
         didSet{
@@ -18,6 +21,13 @@ class RecommandCycleView: UIView {
             collectionView.reloadData()
             // 设置pageControl 页数
             pageControl.numberOfPages = cycleModels?.count ?? 0
+            
+            let indexPath = IndexPath(item: (cycleModels?.count ?? 0) * 10, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .left, animated: false)
+            
+            // 添加定时器前 先移除
+            removeTimer()
+            addTimer()
         }
     }
     @IBOutlet weak var collectionView: UICollectionView!
@@ -30,8 +40,11 @@ class RecommandCycleView: UIView {
         collectionView.register(UINib(nibName: "CycleCell", bundle: nil), forCellWithReuseIdentifier: kCycleViewCellID)
         collectionView.dataSource = self
         collectionView.delegate = self
+        
+        
     }
 
+    /// 布局collectionView layer
     override func layoutSubviews() {
         super.layoutSubviews()
         
@@ -41,23 +54,38 @@ class RecommandCycleView: UIView {
     }
 }
 
-extension RecommandCycleView {
-    class func recommanCycleView() -> RecommandCycleView {
-        return Bundle.main.loadNibNamed("RecommandCycleView", owner: nil, options: nil)?.first as! RecommandCycleView
+// MARK: - 定时器相关方法
+private extension RecommandCycleView {
+    func addTimer() {
+        timer = Timer(timeInterval: 3.0, target: self, selector: #selector(scrollToNext), userInfo: nil, repeats: true)
+        
+        RunLoop.main.add(timer!, forMode: .commonModes)
     }
+    
+    func removeTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    @objc func scrollToNext() {
+        let currentOffsetX = collectionView.contentOffset.x
+        let offsetX = currentOffsetX + collectionView.bounds.width
+        
+        collectionView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: true)
+    }
+    
 }
 
 extension RecommandCycleView: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cycleModels?.count ?? 0
+        return (cycleModels?.count ?? 0) * 1000
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCycleViewCellID, for: indexPath) as! CycleCell
         
-        cell.cycleModel = cycleModels?[indexPath.item]
+        cell.cycleModel = cycleModels?[indexPath.item % cycleModels!.count]
         return cell
     }
 }
@@ -65,13 +93,21 @@ extension RecommandCycleView: UICollectionViewDataSource {
 extension RecommandCycleView : UICollectionViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offsetX = scrollView.contentOffset.x + scrollView.bounds.width * 0.5
-        pageControl.currentPage = Int(offsetX / scrollView.bounds.width)
+        pageControl.currentPage = Int(offsetX / scrollView.bounds.width) % (cycleModels?.count ?? 1)
         
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        removeTimer()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        addTimer()
     }
 }
 
-extension RecommandCycleView : PageContentViewDelegate {
-    func pageContentView(contentView: PageContentView, progress: CGFloat, sourceIndex: Int, targetIndex: Int) {
-        print(progress,sourceIndex)
+extension RecommandCycleView {
+    class func recommanCycleView() -> RecommandCycleView {
+        return Bundle.main.loadNibNamed("RecommandCycleView", owner: nil, options: nil)?.first as! RecommandCycleView
     }
 }
