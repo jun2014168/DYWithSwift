31539d55d4bbbe6f672f0a4958ec65c4824dc2d8
HxAdmin <hxadmin@sunluludeiMac.local>
2017-03-13 15:52:58 +0800

    采集视频

--- DYTest/DYTest/Classes/Room/Controller/RoomViewController.swift
+++ DYTest/DYTest/Classes/Room/Controller/RoomViewController.swift
@@ -10,24 +10,40 @@ import UIKit
 
 class RoomViewController: UIViewController {
 
+    var anchro : AnchroModel?
+    fileprivate lazy var maskImageView : UIImageView = UIImageView(frame: self.view.bounds)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        view.backgroundColor = UIColor.clear
+        setupUI()
     }
 
 
 }
 
-extension RoomViewController  {
+extension RoomViewController {
     
+    /// 设置UI
+    fileprivate func setupUI() {
+        
+        let blurEffect = UIBlurEffect(style: .light)
+        let effectView = UIVisualEffectView(effect: blurEffect)
+        effectView.frame = maskImageView.bounds
+        maskImageView.addSubview(effectView)
+        guard let icon_URL = URL(string: (anchro?.vertical_src)!) else { return }
+        maskImageView.kf.setImage(with: icon_URL)
+        view.addSubview(maskImageView)
+    }
+}
+
+
+extension RoomViewController  {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         navigationController?.setNavigationBarHidden(true, animated: true)
         
     }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
